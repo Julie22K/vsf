8d3868ecd370464fbaa622a33175bbde20309c6a
Julie22K <yuliakononenko22@gmail.com>
2025-11-27 23:26:59 +0200

    without websockets

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import base64
 import cv2
 import numpy as np
-from fastapi import FastAPI, WebSocket
 from framework.processor import Processor
 
 app = FastAPI()
 processor = Processor()
 
-@app.websocket("/ws")
-async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
-    while True:
-        try:
-            data = await ws.receive_text()
-            img_bytes = base64.b64decode(data.split(",")[1])
-            np_arr = np.frombuffer(img_bytes, np.uint8)
-            frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+class Frame(BaseModel):
+    image: str  # base64 string
 
-            detections = processor.process_frame(frame)
-
-            await ws.send_json(detections)
-
-        except Exception as e:
-            print("WebSocket error:", e)
-            break
+@app.post("/detect")
+async def detect(frame: Frame):
+    img_bytes = base64.b64decode(frame.image.split(",")[1])
+    np_arr = np.frombuffer(img_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    detections = processor.process_frame(img)
+    return detections
